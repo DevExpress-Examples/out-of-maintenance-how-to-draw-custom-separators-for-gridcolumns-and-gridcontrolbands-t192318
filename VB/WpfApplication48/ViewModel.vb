9ed2017696f1e0704ccf6ffb2bd0b1e1a938d392
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WpfApplication48/ViewModel.vb
@@ -0,0 +1,29 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Collections.ObjectModel
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace WpfApplication48
+	Public Class ViewModel
+
+		Private _List As ObservableCollection(Of Task)
+		Public ReadOnly Property List() As ObservableCollection(Of Task)
+			Get
+				_List = New ObservableCollection(Of Task)()
+				For i As Integer = 0 To 19
+					_List.Add(New Task() With {.Name = "Task " & i, .FirstValue = i, .SecondValue = i * i, .IsCompleted = i Mod 2 <> 0})
+				Next i
+				Return _List
+			End Get
+		End Property
+
+		Public Class Task
+			Public Property Name() As String
+			Public Property FirstValue() As Integer
+			Public Property SecondValue() As Integer
+			Public Property IsCompleted() As Boolean
+		End Class
+	End Class
+End Namespace
